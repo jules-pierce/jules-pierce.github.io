82d20cfe32e651e74988b6246d02979efff5bc6d
jules-pierce <jupierce@seas.upenn.edu>
2021-03-02 11:45:53 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './App.css';
-import Form from './Form';
+import LearnForm from './Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import { JournalCode } from 'react-bootstrap-icons';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand>
+            <JournalCode /> {' '}
             Improve your Study Skills!
-          </p>
-        </header>
-        <Form />
+          </Navbar.Brand>
+        </Navbar>
+        <LearnForm />
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
-export default class Form extends React.Component {
+export default class LearnForm extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <div>
-                <p>Text here</p>
-            </div>
+            <Card>
+                <Card.Header>Fill out this form!</Card.Header>
+                <Card.Body>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Label Placeholder</Form.Label>
+                            <Form.Control type="text" placeholder="placeholder" />
+                            <Form.Text className="text-muted"> text stuff</Form.Text>
+                        </Form.Group>
+                    </Form>
+                    <Button variant="outline-primary"> Click here! </Button>
+                </Card.Body>
+            </Card>
         )
     }
 }
