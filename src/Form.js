d5d91376eed72071c2f73e05a16d783cc095fa4b
jules-pierce <jupierce@seas.upenn.edu>
2021-03-12 21:06:09 -0500

    add to list functionality

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default class LearnForm extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { platforms: ["Piazza"], selected: "Select a Platform" };
+
+        this.onPiazzaClick = this.onPiazzaClick.bind(this);
+        this.onCanvasClick = this.onCanvasClick.bind(this);
+        this.onOHClick = this.onOHClick.bind(this);
+    }
+
+    onPiazzaClick() {
+        console.log("piazza clicked");
+        const list = this.state.platforms.concat("Piazza");
+        this.setState({
+            platforms: list,
+            selected: "Piazza"
+        });
+    }
+
+    onCanvasClick() {
+        console.log("canvas clicked");
+        const list = this.state.platforms.concat("Canvas");
+        this.setState({
+            platforms: list,
+            selected: "Canvas"
+        });
+    }
+
+    onOHClick() {
+        console.log("oh clicked");
+        const list = this.state.platforms.concat("OH");
+        this.setState({
+            platforms: list,
+            selected: "OH"
+        });
     }
 
     render() {
@@ -13,14 +46,16 @@ export default class LearnForm extends React.Component {
             <Card>
                 <Card.Header>Fill out this form!</Card.Header>
                 <Card.Body>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>Label Placeholder</Form.Label>
-                            <Form.Control type="text" placeholder="placeholder" />
-                            <Form.Text className="text-muted"> text stuff</Form.Text>
-                        </Form.Group>
-                    </Form>
-                    <Button variant="outline-primary"> Click here! </Button>
+                    <ListGroup>
+                        {this.state.platforms.map((platform) => (
+                            <ListGroup.Item>{platform}</ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                    <DropdownButton id="dropdown-basic-button" title={this.state.selected}>
+                        <Dropdown.Item onClick={this.onPiazzaClick}>Piazza</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onCanvasClick}>Canvas</Dropdown.Item>
+                        <Dropdown.Item onClick={this.onOHClick}>OH</Dropdown.Item>
+                    </DropdownButton>
                 </Card.Body>
             </Card>
         )
