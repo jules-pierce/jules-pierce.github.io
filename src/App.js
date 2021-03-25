93fa9a1981f1d68b505cdd0872667fa343a675aa
jules-pierce <jupierce@seas.upenn.edu>
2021-03-25 15:08:07 -0400

    multiple pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,81 @@
 import React from 'react';
 import LearnForm from './Form';
+import Graph from './Graphs';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { JournalCode } from 'react-bootstrap-icons';
+import { Nav } from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
   render() {
     return (
       <div className="App">
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand>
             <JournalCode /> {' '}
-            Improve your Study Skills!
-          </Navbar.Brand>
+             Improve your Study Skills!
+           </Navbar.Brand>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/form">Get Feedback</Nav.Link>
+              <Nav.Link href="/graphs">Graphs</Nav.Link>
+              <Nav.Link href="/about">About Us</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
-        <LearnForm />
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => <LearnForm />}
+            />
+            <Route
+              exact
+              path="/form"
+              render={() => <LearnForm />}
+            />
+            <Route
+              exact
+              path="/graphs"
+              render={() => <Graph />}
+            />
+            <Route
+              exact
+              path="/about"
+              render={() => <About />}
+            />
+          </Switch>
+        </Router>
       </div>
-    );
+    )
   }
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <Navbar bg="dark" variant="dark">
+  //         <Navbar.Brand>
+  //           <JournalCode /> {' '}
+  //           Improve your Study Skills!
+  //         </Navbar.Brand>
+  //         <Navbar.Collapse id="basic-navbar-nav">
+  //           <Nav className="mr-auto">
+  //             <Nav.Link>Test</Nav.Link>
+  //           </Nav>
+  //         </Navbar.Collapse>
+  //       </Navbar>
+  //       <LearnForm />
+  //     </div>
+  //   );
+  // }
 }
---
+++ src/Graphs.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Accordion, Card, Button } from "react-bootstrap";
+
+export default class Graph extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Accordion>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                            Click here
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body> graphs here</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+                <Card>
+                    <Card.Header>
+                        <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                            Click here 2
+                        </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey="1">
+                        <Card.Body> graphs here 2</Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+        )
+    }
+}
